e9d10a33376edfe0c3c929cc3100070fd3991914
Nicolas <nicolas@infolinesystems.com.br>
2022-04-12 17:27:13 -0300

    todas as operacoes

--- src/programa/ExemploPrograma.java
+++ src/programa/ExemploPrograma.java
@@ -3,7 +3,9 @@ package programa;
 import java.util.Scanner;
 
 import classes.Divisao;
+import classes.Multiplicacao;
 import classes.Soma;
+import classes.Subtracao;
 import interfaces.ICalculadora;
 
 public class ExemploPrograma {	
@@ -13,23 +15,26 @@ public class ExemploPrograma {
 		
 		int opc = 0;
 		while(opc != 5) {
-			System.out.println("Digite a operação que deseja ? [1 - Soma, 2 - Divisao, 5 - Sair]");
-			opc = scan.nextInt(); // le a entrada do terminal
-			
+			System.out.println("Digite a operação que deseja ? [1 - Soma, 2 - Subtracao, 3 - Multiplicacao, 4 - Divisao, 5 - Sair");
+			opc = scan.nextInt();
+			ICalculadora calculadora = null;
 			if(opc == 1 ) {
-				ICalculadora calculadora = new Soma();
-				Float result = calculadora.calcula((float)1.0, (float)2.0);
-				System.out.println(result);
-	//		ICalculadora calculadora = new Subtracao();
+				calculadora = new Soma();
 			} else if (opc == 2) {
-				ICalculadora calculadora = new Divisao();
-				Float result = calculadora.calcula((float)1.0, (float)2.0);
+				calculadora = new Subtracao();
+			}else if (opc == 3) {
+				calculadora = new Multiplicacao();
+			}else if (opc == 4) {
+				calculadora = new Divisao();
+			}
+			if(calculadora != null) {
+				System.out.println("Digite 2 valores:");
+				Integer n1 = scan.nextInt(); 
+				Integer n2 = scan.nextInt();
+				Integer result = calculadora.calcula(n1, n2);
 				System.out.println(result);
 			}
 		}
-//		ICalculadora calculadora = new Multiplicacao();
-//		
 		scan.close();
-		
 	}
 }
